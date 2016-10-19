7165740f5a53098c560909c00f4894ae425ccb04
Rafael Zabeu Spessotto <rafaelzabeu@gmail.com>
2016-10-19 18:55:29 -0200

    ionic. detalhes dos Projects

--- app/app-evidencia/src/pages/project-page/project-page.ts
+++ app/app-evidencia/src/pages/project-page/project-page.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {ProjectService} from '../../providers/project-service';
+import {BaseService} from '../../providers/base-service';
+import {Project} from '../../models/Project';
+import {ProjectDetails} from '../project-details/project-details';
 /*
   Generated class for the ProjectPage page.
 
@@ -9,11 +12,23 @@ import { NavController } from 'ionic-angular';
 */
 @Component({
   selector: 'page-project-page',
-  templateUrl: 'project-page.html'
+  templateUrl: 'project-page.html',
+  providers: [BaseService,ProjectService]
 })
 export class ProjectPage {
 
-  constructor(public navCtrl: NavController) {}
+  public projects : Project[];
+
+  constructor(public navCtrl: NavController, private projectService: ProjectService) {
+      this.projectService.get().subscribe(res => {
+          this.projects = res;
+      });
+  }
+
+  goToDetails(id)
+  {
+      this.navCtrl.push(ProjectDetails,{_id: id});
+  }
 
   ionViewDidLoad() {
     console.log('Hello ProjectPage Page');
