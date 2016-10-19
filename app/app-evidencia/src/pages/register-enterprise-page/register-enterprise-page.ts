a2bd9f8e5435eb3c8b3cc428c0315596260b809f
rafaelzabeu <rafaelzabeu@gmail.com>
2016-10-19 15:47:07 -0200

    ionic. Cadastro enterprise

--- app/app-evidencia/src/pages/register-enterprise-page/register-enterprise-page.ts
+++ app/app-evidencia/src/pages/register-enterprise-page/register-enterprise-page.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Validators, FormBuilder } from '@angular/forms';
+import {BaseService} from '../../providers/base-service';
+import {EnterpriseService} from '../../providers/enterprise-service';
+import {Enterprise} from '../../models/Enterprise';
 @Component({
   selector: 'page-register-enterprise-page',
-  templateUrl: 'register-enterprise-page.html'
+  templateUrl: 'register-enterprise-page.html',
+  providers: [BaseService,EnterpriseService]
 })
 export class RegisterEnterprisePage {
 
   public registrationForm:any;
 
-    constructor(private formBuilder: FormBuilder) {}
+    constructor(private formBuilder: FormBuilder, private enterpriseService : EnterpriseService) {
+        this.enterpriseService.get().subscribe(res => {
+          console.log(res);
+        });
+    }
 
     ionViewDidLoad() {
       this.registrationForm = this.formBuilder.group({
@@ -24,7 +32,16 @@ export class RegisterEnterprisePage {
       });
     }
     logForm(){
-      console.log(this.registrationForm.value)
+      console.log(this.registrationForm.value);
+      let enterprise = new Enterprise();
+      enterprise.Name = this.registrationForm.value.Name;
+      enterprise.CNPJ = this.registrationForm.value.CNPJ;
+      enterprise.Login = this.registrationForm.value.Login;
+      enterprise.Password = this.registrationForm.value.Password;
+      enterprise.Description = this.registrationForm.value.Description;
+      enterprise.Categories = this.registrationForm.value.Categories;
+      enterprise.Website = this.registrationForm.value.Website;
+      this.enterpriseService.post(enterprise);
     }
 
 }
