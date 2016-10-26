ecd91df3f47d3283ef287ff9fe3808f2ef32c1ce
Guilherme <guilherme.dimarchi@outlook.com>
2016-10-26 21:57:49 -0200

    Alterado logica de login api conforme combinado entre Dimarchi e Rodolfo

---
+++ api/api-evidencias/app/api/Login.js
@@ -0,0 +1,96 @@
+var mongoose = require('mongoose');
+var util = require('util.js');
+var jwt  = require('jwt-simple');
+var api = {}
+var model = mongoose.model('Login');
+var config = require('../../config/config');
+
+api.get = function(req, res) {  
+	res = util.setResponse(res);
+	model.find({})
+	.then(function(logins){
+		res.json(logins);
+	}, function(error){
+		console.log(error);
+		res.status(500).json(error);
+	});  
+};
+
+api.getById = function(req, res) {
+    res = util.setResponse(res);
+	model.findById(req.params.id)
+    .then(function(login){
+		if(!login) throw Error('Login não encontrado')
+			res.json(login);      
+    }, function(error){
+		console.log(error);
+        res.status(500).json(error);
+    })        
+};
+
+api.delete = function(req, res) {
+	res = util.setResponse(res);
+    model.remove({_id : req.params.id})
+    .then(function(){
+        res.sendStatus(204);
+    }, function(error){
+        console.log(error);
+        res.status(500).json(error);
+    })       
+};
+
+api.post = function(req, res){
+	res = util.setResponse(res);
+    var login = req.body;
+
+    model.create(login)
+    .then(function(login){
+        res.json(login);
+    }, function(error){
+        console.log(error);
+        res.status(500).json(error);
+    })
+}
+
+api.put = function(req, res){
+	res = util.setResponse(res);
+	model.findByIdAndUpdate(req.params.id, req.body)
+	.then(function(){
+		res.json(login);
+	}, function(error){
+		console.log(error);
+        res.status(500).json(error);
+    })
+}
+
+api.auth = function(req, res){
+
+//console.log(req.body);
+  model.findOne({
+    Login: req.body.Login
+  }, function(err, login) {
+    if (err) throw err;
+ 
+    if (!login) {
+      res.send({success: false, msg: 'Authentication failed. User not found.'});
+    } else {
+      // check if password matches
+      login.comparePassword(req.body.Password, function (err, isMatch) {
+        if (isMatch && !err) {
+          // if user is found and password is right create a token
+          var token = jwt.encode(login, config.secret);
+          // return the information including token as JSON
+          //console.log(login);
+          res.json({success: true, token: 'JWT ' + token, type: login.Type, loginId: login.ReferenceId});
+        } else {
+          res.send({success: false, msg: 'Authentication failed. Wrong password.'});
+        }
+      });
+    }
+  });
+
+
+}
+
+
+module.exports = api;
--- api/api-evidencias/app/models/User.js
+++ api/api-evidencias/app/models/User.js
@@ -31,14 +31,6 @@ var schema = mongoose.Schema({
     About: {
         type: String
     },
-    Login: {
-        type: String,
-        required: true
-    },
-    Password: {
-        type: String,
-        required: true,
-    },
     RegistrationDate: {
         type: Date,
         default: Date.now
@@ -46,39 +38,7 @@ var schema = mongoose.Schema({
 });
 
 
-schema.pre('save', function (next) {
-    var user = this;
-   // console.log(user);
-    if (this.isModified('Password') || this.isNew) {
-        bcrypt.genSalt(10, function (err, salt) {
-            if (err) {
-                return next(err);
-            }
-            bcrypt.hash(user.Password, salt, function (err, hash) {
-                if (err) {
-                    return next(err);
-                }
-                user.Password = hash;
-                next();
-            });
-        });
-    } else {
-        return next();
-    }
-});
- 
-schema.methods.comparePassword = function (passw, cb) {
-  //  console.log(passw);
- //   console.log(this.Password);
 
-    bcrypt.compare(passw, this.Password, function (err, isMatch) {
-        if (err) {
-            return cb(err);
-        }
-        cb(null, isMatch);
-    });
-};
- 
 
 
 module.exports = mongoose.model('User', schema);
